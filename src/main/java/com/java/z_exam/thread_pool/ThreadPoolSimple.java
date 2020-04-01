a4d2ec75c08dd7a2db27297f87556e5d9e88d53f
leizuquan <843291011@qq.com>
2020-04-01 10:24:01 +0800

    # 新增 优化线程池调优

--- src/main/java/com/java/z_exam/thread_pool/ThreadPoolSimple.java
+++ src/main/java/com/java/z_exam/thread_pool/ThreadPoolSimple.java
@@ -1,6 +1,7 @@
 package com.java.z_exam.thread_pool;
 
-import java.util.concurrent.ArrayBlockingQueue;
+import java.time.LocalTime;
+import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
@@ -12,6 +13,10 @@ import java.util.concurrent.TimeUnit;
  * @Modified By:
  * @Description:
  */
+
+/**
+ * -Xms10m -Xmx10m -XX:+PrintGCDetails -XX:+PrintCommandLineFlags -XX:+UseParallelGC -XX:+UseParallelOldGC
+ */
 public class ThreadPoolSimple {
     public static void main(String[] args) {
         // corePoolSize 核心线程数，可长期驻留的线程数量
@@ -21,26 +26,30 @@ public class ThreadPoolSimple {
         // 线程池pool的阻塞队列是ArrayBlockingQueue，ArrayBlockingQueue是一个有界的阻塞队列，ArrayBlockingQueue的容量为1。这也意味着线程池的阻塞队列只能有一个线程池阻塞等待。
 
         ThreadPoolExecutor executor = new ThreadPoolExecutor(
+                3,
                 5,
-                10,
-                200,
-                TimeUnit.MILLISECONDS,
-                new ArrayBlockingQueue<>(5));
+                2,
+                TimeUnit.SECONDS,
+                //new ThreadFactoryBuilder().setNameFormat("getP5wData-task-%d").build(),
+                //new ArrayBlockingQueue<>(5));
+                new LinkedBlockingQueue<>(10),
+                new ThreadPoolExecutor.CallerRunsPolicy());
 
         // ThreadPoolExecutor.AbortPolicy:丢弃任务并抛出RejectedExecutionException异常。
         // ThreadPoolExecutor.DiscardPolicy：也是丢弃任务，但是不抛出异常。
         // ThreadPoolExecutor.DiscardOldestPolicy：丢弃队列最前面的任务，然后重新尝试执行任务（重复此过程）
         // ThreadPoolExecutor.CallerRunsPolicy：由调用线程处理该任务
         // 设置线程池的拒绝策略为"丢弃"
-        executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
 
         //for (int i = 0; i < 15; i++) {
-        for (int i = 0; i < 20; i++) {
+        for (int i = 0; i < Long.MAX_VALUE; i++) {
             MyTask myTask = new MyTask(i);
             executor.execute(myTask);
-            System.out.println("线程池中线程数目：" + executor.getPoolSize()
-                + ", 队列中等待执行的任务数目：" + executor.getQueue().size()
-                + ", 已执行完别的任务数目：" + executor.getCompletedTaskCount());
+            System.out.println(
+                    LocalTime.now() + "\t"
+                            + "线程池中线程数目：" + executor.getPoolSize()
+                            + ", 队列中等待执行的任务数目：" + executor.getQueue().size()
+                            + ", 已执行完别的任务数目：" + executor.getCompletedTaskCount());
         }
 
         executor.shutdown();
@@ -50,21 +59,24 @@ public class ThreadPoolSimple {
 
 class MyTask implements Runnable{
 
+    //byte[] a = new byte[1024 * 100];  // 100KB
     private int taskNum;
 
     public MyTask(int taskNum) {
+
         this.taskNum = taskNum;
     }
 
     @Override
     public void run() {
-        System.out.println("正在执行task " + taskNum);
+        //System.out.println("正在执行task " + taskNum);
 
         try {
-            Thread.currentThread().sleep(4000);
+            //Thread.currentThread().sleep(4000);
+            TimeUnit.MILLISECONDS.sleep(10);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        System.out.println("task " + taskNum + " 执行完毕");
+        //System.out.println("task " + taskNum + " 执行完毕");
     }
 }
