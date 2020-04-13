58b0dc2f8fbc7b736ca3de9f51071f22e7561704
leizuquan1990 <843291011@qq.com>
2020-04-13 22:41:17 +0800

    # 新增java程序调试工具

---
+++ src/main/java/com/java/z_exam/debug_tool/JavaDebugWays.java
@@ -0,0 +1,32 @@
+package com.java.z_exam.debug_tool;
+
+/**
+ * @Author: Lei
+ * @E-mail: 843291011@qq.com
+ * @Date: Created in 10:29 下午 2020/4/13
+ * @Version: 1.0
+ * @Modified By:
+ * @Description:
+ */
+
+/**
+ * JAVA 调试工具
+ * 注意权限问题
+ *      jps
+ *          输出java 进程id以及启动参数
+ *      jstack
+ *          输出java 进程中各线程状态
+ *          会检测线程间是否有死锁
+ *      jmap
+ *          dump java进程内存状态快照
+ *      jhat
+ *          可以建立用于分析jmap dump出的快照文件的web ui
+ *          内存泄漏分析
+ *      gc log
+ *
+ *      jstat
+ *          实时输出堆内存各区域使用情况
+ */
+public class JavaDebugWays {
+
+}
