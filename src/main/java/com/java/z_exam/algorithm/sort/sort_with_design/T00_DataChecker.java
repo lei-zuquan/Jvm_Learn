9867349ef5b3c3bc0320bb8fe186b5a487cfe82b
leizuquan <843291011@qq.com>
2020-05-29 09:07:22 +0800

    # 新增通用型排序校对器

---
+++ src/main/java/com/java/z_exam/algorithm/sort/sort_with_design/T00_DataChecker.java
@@ -0,0 +1,55 @@
+package com.java.z_exam.algorithm.sort.sort_with_design;
+
+import java.util.Arrays;
+import java.util.Random;
+
+/**
+ * @Author: Lei
+ * @E-mail: 843291011@qq.com
+ * @Date: 2020-05-29 9:03
+ * @Version: 1.0
+ * @Modified By:
+ * @Description:
+ */
+public class T00_DataChecker {
+
+    static Integer[] generateRandomArray() {
+        Random r = new Random();
+        Integer[] arr = new Integer[10000];
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = r.nextInt(10000);
+        }
+
+        return arr;
+    }
+
+    static void check() {
+        boolean same = true;
+
+        for (int times = 0; times < 10; times++) {
+            Integer[] arr = generateRandomArray();
+            Integer[] arr2 = new Integer[arr.length];
+            System.arraycopy(arr, 0, arr2, 0, arr.length);
+
+            Arrays.sort(arr);
+            T03_Insertion.sort(arr2);
+
+            for (int i = 0; i < arr2.length; i++) {
+                if (arr[i] != arr2[i]) {
+                    same = false;
+                    break;
+                }
+            }
+
+            if (!same) break;
+        }
+
+
+        System.out.println(same == true ? "right" : "wrong");
+    }
+
+    public static void main(String[] args) {
+        check();
+    }
+}
