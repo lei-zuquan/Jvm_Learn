8c4d5062679973813e7e39cd6165595f2f169329
leizuquan1990 <843291011@qq.com>
2020-08-16 09:25:04 +0800

    # 新增编译期处理——默认构造器

---
+++ src/main/java/com/jvm/t08_compile_time/T01_CompileTime_DefaultConstructor.java
@@ -0,0 +1,15 @@
+package com.jvm.t08_compile_time;
+
+/**
+ * @Author: Lei
+ * @E-mail: 843291011@qq.com
+ * @Date: Created in 8:00 上午 2020/8/16
+ * @Version: 1.0
+ * @Modified By:
+ * @Description:
+ */
+
+// 编译期处理——默认构造器
+public class T01_CompileTime_DefaultConstructor {
+
+}
