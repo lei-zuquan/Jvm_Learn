ffe019c5c1638b88220768e2293e51451fb49234
leizuquan1990 <843291011@qq.com>
2020-08-17 14:08:11 +0800

    # 新增：类加载分析 - 演示 final 对静态变量的影响

---
+++ src/main/java/com/jvm/t09_class_load/T01_ClassLoad_HelloWorld.java
@@ -0,0 +1,17 @@
+package com.jvm.t09_class_load;
+
+/**
+ * @Author: Lei
+ * @E-mail: 843291011@qq.com
+ * @Date: Created in 1:06 下午 2020/8/17
+ * @Version: 1.0
+ * @Modified By:
+ * @Description:
+ */
+
+// 类加载分析 - 加载
+public class T01_ClassLoad_HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld");
+    }
+}
---
+++ src/main/java/com/jvm/t09_class_load/T02_ClassLoad_Final_Static.java
@@ -0,0 +1,18 @@
+package com.jvm.t09_class_load;
+
+/**
+ * @Author: Lei
+ * @E-mail: 843291011@qq.com
+ * @Date: Created in 1:07 下午 2020/8/17
+ * @Version: 1.0
+ * @Modified By:
+ * @Description:
+ */
+// 类加载分析 - 演示 final 对静态变量的影响
+public class T02_ClassLoad_Final_Static {
+    static int a;
+    static int b = 10;
+//    static final int c = 20;
+//    static final String d = "hello";
+//    static final Object e = new Object();
+}
