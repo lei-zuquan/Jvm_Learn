031e5d644a959f935971de8940b5b8ad9908a207
leizuquan1990 <843291011@qq.com>
2020-08-18 08:30:07 +0800

    # 新增：用启动类加载器 Bootstrap加载类

---
+++ src/main/java/com/jvm/t10_class_loader/T01_ClassLoader_Bootstrap.java
@@ -0,0 +1,27 @@
+package com.jvm.t10_class_loader;
+
+/**
+ * @Author: Lei
+ * @E-mail: 843291011@qq.com
+ * @Date: Created in 7:20 上午 2020/8/18
+ * @Version: 1.0
+ * @Modified By:
+ * @Description:
+ */
+
+// 用启动类加载器 Bootstrap加载类
+public class T01_ClassLoader_Bootstrap {
+    public static void main(String[] args) throws ClassNotFoundException {
+        // forName 可以完成类的加载，也可以类的链接、初始化操作
+        Class<?> aClass = Class.forName("com.jvm.t10_class_loader.F");
+        System.out.println(aClass.getClassLoader());
+    }
+}
+
+class F {
+    static {
+        System.out.println("bootstrap F init");
+    }
+}
+
+
