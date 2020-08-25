bc09c0dde261e0fcd20f4647d66524bfd104ef08
leizuquan <843291011@qq.com>
2020-08-25 09:43:11 +0800

    # 更正冒泡排序

--- src/main/java/com/java/z_exam/algorithm/c02_sort/T_0003_DataChecker.java
+++ src/main/java/com/java/z_exam/algorithm/c02_sort/T_0003_DataChecker.java
@@ -36,12 +36,12 @@ public class T_0003_DataChecker {
 
             Arrays.sort(arr);
             //T_0002_SelectionSort.selectSort(arr2);
-            //T_0004_BubbleSort.bubbleSort(arr2);
+            T_0004_BubbleSort.bubbleSort(arr2);
             //T_0005_InsertionSort.insertionSort(arr2);
             //T_0006_ShellSort.shellSort(arr2);
             //T_0007_MergeSort.mergeSort(arr2, 0, arr2.length - 1);
             //T_0008_QuickSort.quickSort(arr2, 0, arr2.length - 1);
-            T_0009_CountSort.countSort(arr2);
+            //T_0009_CountSort.countSort(arr2);
 
             for (int i = 0; i < arr2.length; i++) {
                 if (arr[i] != arr2[i]) {
--- src/main/java/com/java/z_exam/algorithm/c02_sort/T_0004_BubbleSort.java
+++ src/main/java/com/java/z_exam/algorithm/c02_sort/T_0004_BubbleSort.java
@@ -75,22 +75,6 @@ public class T_0004_BubbleSort {
         }
     }
 
-
-    public static void sort(int[] a) {
-        for (int i = a.length - 1; i > 0; i--) {
-            for (int j = 0; j < i; j++) {
-                if (a[j] > a[j + 1]) //swap(a, j, j+1);
-                    findMax(a, i);
-            }
-        }
-    }
-
-    private static void findMax(int[] a, int n) {
-        for (int j = 0; j < n; j++) {
-            if (a[j] > a[j+1]) swap(a, j, j+1);
-        }
-    }
-
     private static void swap(int[] arr, int i, int j){
         int temp = arr[i];
         arr[i] = arr[j];
