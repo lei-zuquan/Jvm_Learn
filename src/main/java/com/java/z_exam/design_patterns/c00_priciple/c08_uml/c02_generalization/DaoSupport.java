1b6f21571001ba761b7d09ea8936065646410c3f
leizuquan1990 <843291011@qq.com>
2020-08-30 22:23:30 +0800

    # 新增UML类图——泛化关系

---
+++ src/main/java/com/java/z_exam/design_patterns/c00_priciple/c08_uml/c02_generalization/DaoSupport.java
@@ -0,0 +1,14 @@
+package com.java.z_exam.design_patterns.c00_priciple.c08_uml.c02_generalization;
+
+/**
+ * @Author:
+ * @Date: Created in 10:16 下午 2020/8/30
+ * @Version: 1.0
+ * @Modified By:
+ * @Description:
+ */
+public abstract class DaoSupport{
+    public void save(Object entity){}
+
+    public void delete(Object id){}
+}
---
+++ src/main/java/com/java/z_exam/design_patterns/c00_priciple/c08_uml/c02_generalization/PersonServiceBean.java
@@ -0,0 +1,11 @@
+package com.java.z_exam.design_patterns.c00_priciple.c08_uml.c02_generalization;
+
+/**
+ * @Author:
+ * @Date: Created in 10:17 下午 2020/8/30
+ * @Version: 1.0
+ * @Modified By:
+ * @Description:
+ */
+public class PersonServiceBean extends DaoSupport{
+}
