fa025871e597cb44f554212e383d381ad272662e
leizuquan1990 <843291011@qq.com>
2020-09-26 17:16:58 +0800

    # 新增二叉搜索树转化为链表

---
+++ src/main/java/com/test/tree/BST2List.java
@@ -0,0 +1,120 @@
+package com.test.tree;
+
+/**
+ * @Author:
+ * @Date: Created in 4:00 下午 2020/9/26
+ * @Version: 1.0
+ * @Modified By:
+ * @Description:
+ */
+//将二叉搜索树转化为有序的双向链表, 不需要额外的辅助空间。
+public class BST2List {
+
+    private class BinaryNode implements Comparable<BinaryNode>{
+        int ele;
+        BinaryNode left;
+        BinaryNode right;
+
+        public BinaryNode(int ele) {
+            this.ele = ele;
+            left = right = null;
+        }
+
+        @Override
+        public int compareTo(BinaryNode node) {
+            return this.ele - node.ele;
+        }
+
+        @Override
+        public String toString() {
+            return this.ele + " ";
+        }
+
+    }
+
+    private BinaryNode root;
+
+    public void buildTree(int[] ele){
+        for (int i : ele) {
+            insert(i);
+        }
+    }
+    private void insert(int ele){
+        root = insert(root, ele);
+    }
+    private BinaryNode insert(BinaryNode root, int ele){
+        if(root == null)
+            return new BinaryNode(ele);
+        if(root.ele > ele)
+            root.left = insert(root.left, ele);
+        else if(root.ele < ele)
+            root.right = insert(root.right, ele);
+        return root;
+    }
+
+    //将二叉搜索树转化成双向链表,返回链表的头结点
+    public BinaryNode bstConvert2List(BinaryNode root){
+        if(root == null)
+            return null;
+
+        BinaryNode lastNode = null;
+        lastNode = convertNode(root, lastNode);
+
+        // 回到链表最左，表示链表最小节点，为了遍历方便一直向右遍历
+        BinaryNode head = null;
+        while(lastNode != null)
+        {
+            head = lastNode;
+            lastNode = lastNode.left;
+        }
+        return head;
+    }
+    /**
+     * 采用中序遍历思想将二叉搜索树的结点转化为双向链表的结点
+     * @param root 从二叉搜索树的根开始转换
+     * @param lastMinNode 当前链表的最后一个结点
+     * @return 链表的最后一个结点
+     */
+    private BinaryNode convertNode(BinaryNode root, BinaryNode lastMinNode){
+        if(root == null)
+            return null;
+
+        BinaryNode currentNode = root;//当前待转换子树的根结点
+        if(root.left != null)
+            lastMinNode = convertNode(root.left, lastMinNode); //向根的左子树转换(类似于中序遍历中左子树遍历)
+
+        currentNode.left = lastMinNode;
+
+        if(lastMinNode != null) // 因为第一次是null，后续通过中序遍历找到当前最小值
+            lastMinNode.right = currentNode;
+        // todo: 通过对搜索二叉树进行中序遍历即得到从小到大的排序，这样每次只需要将最小的数据
+        lastMinNode = currentNode;// 将currentNode加入到链表中
+
+        if(root.right != null)
+            lastMinNode = convertNode(root.right, lastMinNode);//往根的右子树转换(类似于中序遍历中右子树遍历)
+
+        return lastMinNode;
+    }
+
+    public void printList(BinaryNode head){
+        if(head == null)
+            return;
+        BinaryNode current = head;
+        while(current != null)
+        {
+            System.out.print(current);
+            current = current.right;
+        }
+    }
+
+    //hapjin test
+    public static void main(String[] args) {
+        //int[] eles = {10,6,14,4,8,12,16};
+        int[] eles = {2,3,1};
+        BST2List obj = new BST2List();
+        obj.buildTree(eles);
+
+        BinaryNode head = obj.bstConvert2List(obj.root);
+        obj.printList(head);
+    }
+}
---
+++ src/main/java/com/test/tree/C113.java
@@ -0,0 +1,101 @@
+package com.test.tree;
+
+/**
+ * @Author:
+ * @Date: Created in 12:09 下午 2020/9/26
+ * @Version: 1.0
+ * @Modified By:
+ * @Description:
+ */
+
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
+
+
+/**
+         [5]
+        / \
+     [4]   [8]
+      /   / \
+    [11]  13 [4]
+    /  \     / \
+   7  [2]  [5]  1
+
+ 给定一个二叉树和一个目标和，找到所有从根节点到叶子节点路径总和等于给定目标和的路径。
+ */
+public class C113 {
+    LinkedList<List<Integer>> ret = new LinkedList<List<Integer>>();
+    LinkedList<Integer> path = new LinkedList<Integer>();
+
+    public List<List<Integer>> pathSum(TreeNode root, int sum) {
+        dfs(root, sum);
+        return ret;
+    }
+
+    private void dfs(TreeNode root, int sum) {
+        if (root == null) {
+            return;
+        }
+
+        sum -= root.val;
+        path.offerLast(root.val);
+        if (root.left == null && root.right == null && sum == 0) {
+            ret.add(new LinkedList<>(path));
+        }
+
+        dfs(root.left, sum);
+        dfs(root.right, sum);
+
+        path.pollLast();
+    }
+
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(5);
+        TreeNode l1Left = new TreeNode(4);
+        TreeNode l1Right = new TreeNode(8);
+
+        TreeNode l2Left = new TreeNode(11);
+        TreeNode l2Left2 = new TreeNode(13);
+        TreeNode l2Right = new TreeNode(4);
+
+        TreeNode l3Left = new TreeNode(7);
+        TreeNode l3Right = new TreeNode(2);
+        TreeNode l3Left2 = new TreeNode(5);
+        TreeNode l3Right2 = new TreeNode(1);
+
+        root.left = l1Left;
+        root.right = l1Right;
+
+        l1Left.left = l2Left;
+        l1Right.left = l2Left2;
+        l1Right.right = l2Right;
+
+        l2Left.left = l3Left;
+        l2Left.right = l3Right;
+
+        l2Right.left = l3Left2;
+        l2Right.right = l3Right2;
+
+        C113 c113 = new C113();
+        List<List<Integer>> lists = c113.pathSum(root, 22);
+
+        for (int i = 0; i < lists.size(); i++) {
+            List<Integer> list = lists.get(i);
+            for (int j = 0; j < list.size(); j++) {
+                System.out.print(list.get(j) + "\t");
+            }
+            System.out.println("============= way:" + (i+1));
+        }
+
+    }
+}
